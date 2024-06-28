a866c111d828350ad765e8cbe2914d8dae7859b3
v1xingyue <qixingyue@gmail.com>
2024-06-28 18:32:37 +0800

    add anyhoow

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,15 +1,16 @@
 use {
+    anyhow::{Ok, Result},
     clap::Parser,
     log::{error, info},
     rustapp_template::{app, cli, config},
 };
 
-fn main() {
+fn main() -> Result<()> {
     let mut mycli: cli::Cli = cli::Cli::parse();
     if mycli.name.is_none() && mycli.command.is_none() {
         error!("Please input the command");
         cli::Cli::parse_from(&["rustapp", "--help"]);
-        return;
+        return Err(anyhow::anyhow!("Please input the command"));
     }
     let mut config = config::MyConfig::load(&mycli.config, true);
     app::start(
@@ -24,10 +25,12 @@ fn main() {
     match mycli.command {
         Some(cli::Command::Test { list }) => {
             info!("list value is :  {}", list);
+            Ok(())
         }
         _ => {
-            error!("Invalid command");
+            error!("Please input the command");
             cli::Cli::parse_from(&["rustapp", "--help"]);
+            Err(anyhow::anyhow!("Please input the command"))
         }
     }
 }
